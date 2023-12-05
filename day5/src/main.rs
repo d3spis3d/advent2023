8083dc839d77df59dfdd330ecdd3a14ff8338e68
Daniel Budden <daniel.l.budden@gmail.com>
2023-12-06 00:19:08 +1100

    Still wip day5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -21,4 +22,42 @@ fn main() {
         .trim_start_matches("seeds: ")
         .split(" ")
         .collect::<Vec<&str>>();
+
+    let mut blank = lines.next().unwrap().unwrap(); // skip blank
+    if blank != "" {
+        println!("{}", blank);
+        panic!("argggg not blank");
+    }
+
+    let seed_to_soil_header = lines.next().unwrap().unwrap();
+    if seed_to_soil_header != "seed-to-soil map:" {
+        panic!("bad header");
+    }
+
+    let mut seed_to_soil: HashMap<u32, u32> = HashMap::new();
+
+    let re = Regex::new(r"^(?<original>[0-9]+) (?<new>[0-9]+) (?<range>[0-9]+)$").unwrap();
+
+    loop {
+        let l = lines.next().unwrap().unwrap();
+        if l == "" {
+            break;
+        }
+
+        let caps = re.captures(&l).unwrap();
+        let seed = caps
+            .name("original")
+            .unwrap()
+            .as_str()
+            .parse::<u32>()
+            .unwrap();
+        let soil = caps.name("new").unwrap().as_str().parse::<u32>().unwrap();
+        let range = caps.name("range").unwrap().as_str().parse::<u32>().unwrap();
+
+        for i in 0..range {
+            seed_to_soil.insert(seed + i, soil + i);
+        }
+    }
+
+    // make that into a function to call for each section
 }
